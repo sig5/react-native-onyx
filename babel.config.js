55709afd63ac8dd14bd41db389ac7f83ef7a5b13
Sakar Singhal <48352577+sig5@users.noreply.github.com>
2022-01-18 01:31:21 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-    presets:[
-        "@babel/preset-env",
-        "@babel/preset-react"
-    ],
-    plugins: ["@babel/plugin-syntax-jsx"],
-};
+ module.exports = {
+        presets: [
+          '@babel/preset-react',
+          [ 
+            '@babel/preset-env',
+            {
+              targets: {
+                node: 'current',
+              },
+            },
+          ],
+        ],
+      };
